20c00a36123e9f8c3550ab3248c9c0ce337c7df9
Wasith Theerapattrathamrong <phai@live.com>
2019-04-11 16:09:23 +0700

    test

--- lib/fastlane/plugin/line_message/actions/line_message_action.rb
+++ lib/fastlane/plugin/line_message/actions/line_message_action.rb
@@ -18,10 +18,7 @@ module Fastlane
 
         request = Net::HTTP::Post.new(uri.request_uri)
         request["Authorization"] = "Bearer #{api_token}"
-        request.set_form_data({
-          :to =>  to,
-          :messages => params[:messages]
-        }.to_json)
+        request.set_form_data({:make => "bmw", :year => "2003"}.to_json)
 
         request["Content-Type"] = "application/json"
 
